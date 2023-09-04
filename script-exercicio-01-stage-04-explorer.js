9a6ea465e9b661c1f048e05ae0b3ff18cddc5184
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-09-04 16:37:23 -0300

    Update script-exercicio-01-stage-04-explorer.js

--- script-exercicio-01-stage-04-explorer.js
+++ script-exercicio-01-stage-04-explorer.js
@@ -25,18 +25,21 @@ let divisao = primeiroNumero / segundoNumero
 let restoDivisao = primeiroNumero % segundoNumero
 
 function parOuImpar(numero){
-    let par = 'os números são impares'
+    let par = 'impar'
     if(numero % 2 === 0){
-        par = 'os números são pares'
-    }    
+        par = 'par'
+    }   
+    
+    return par
 }
 
 
 function verificarIgualdade(numero1, numero2){
     let iguais = 'Os números são diferentes'
-    if(numero1 === numero1){
+    if(numero1 === numero2){
         iguais = "Os números são iguais"
     }
+    return iguais
 }
 
 console.log(soma)
@@ -44,3 +47,13 @@ console.log(subtracao)
 console.log(multiplicacao)
 console.log(divisao)
 console.log(restoDivisao)
+
+
+alert(`A soma dos dois números é: ${soma}`)
+alert(`A subtração dos dois números é: ${subtracao}`)
+alert(`A multiplicação dos dois números é: ${multiplicacao}`)
+alert(`A divisão dos dois números é: ${divisao}`)
+alert(`O resto da divisão dos dois números é: ${restoDivisao}`)
+
+alert(`soma dos dois números é: ${parOuImpar(soma)}`)
+alert(verificarIgualdade(primeiroNumero, segundoNumero))
